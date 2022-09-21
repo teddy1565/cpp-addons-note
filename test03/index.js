1beffda27fba1cb941d2e048ee1cdf14730ba23b
teddy1565 <teddy1565@gmail.com>
2022-09-21 14:25:01 +0800

    簡化測試

--- test03/index.js
+++ test03/index.js
@@ -1,5 +1,5 @@
 const arithmetic = require("./build/Release/arithmetic");
-console.log(arithmetic.arithmetic("15 + 6"));
+console.log(arithmetic.arithmetic("1 + 15"));
 console.log(arithmetic.arithmetic("15 - 6"));
 console.log(arithmetic.arithmetic("15 * 6"));
 console.log(arithmetic.arithmetic("15 / 6"));
